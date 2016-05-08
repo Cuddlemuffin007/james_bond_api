9d61bfd02926ae726bfa24b480d4946d4c911aff
Cuddlemuffin007 <brennonkrogers@gmail.com>
2016-05-08 16:03:03 -0400

    extracted superuser creds to environment variables

--- api_app/management/commands/createsu.py
+++ api_app/management/commands/createsu.py
@@ -1,8 +1,15 @@
+import os
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
+
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        if not User.objects.filter(username='admin').exists():
-            User.objects.create_superuser('admin', 'admin@admin.com', 'safepass')
+        username = os.environ.get('SU_USERNAME')
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(
+                username,
+                'admin@admin.com',
+                os.environ.get('SU_PASSWORD')
+            )
